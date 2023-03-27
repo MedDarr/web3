8c9d0c0ed0401a90d7844c0e7595771b53d9a090
MedDarr <112845790+MedDarr@users.noreply.github.com>
2023-03-27 19:23:34 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
   <html lang="ru">
     <head>
@@ -21,31 +22,25 @@
           
           <label><br>ГОД:<br>
             <select id="year" name="year">
-                <option value="2000">2000 год</option>
-                <option value="2001">2001 год</option>
-                <option value="2002">2002 год</option>
-                <option value="2003">2003 год</option>
-                <option value="2004">2004 год</option>
-                <option value="2005">2005 год</option>
-                <option value="2006">2006 год</option>
-                <option value="2007">2007 год</option>
-                <option value="2008">2008 год</option>
-                <option value="2009">2009 год</option>
-                <option value="2010">2010 год</option>
-                <option value="2011">2011 год</option>  
-           </label><br>
+            <?php
+                for ($i = 1922; $i <= 2022; $i++) {
+                printf('<option value="%d">%d год</option>', $i, $i);
+                }
+            ?>
+            </select>
+          </label><br>
           
             <label><br>Пол:<br>
-          <input type="radio" name="pol" value="Значение1" >Женский</label>
-          <label><input type="radio" name="pol" value="Значение2" >Мужской</label>
+          <input type="radio" name="pol" value="w" >Женский</label>
+          <label><input type="radio" name="pol" value="m" >Мужской</label>
           
              <label><br>Количество конечностей:<br>
-            <input type="radio"  name="kolvo" value="Значение1" >2</label>
-            <label><input type="radio" name="kolvo" value="Значение3" >3</label>
-            <label><input type="radio" name="kolvo" value="Значение4" >4</label>
+            <input type="radio"  name="kolvo" value="2" >2</label>
+            <label><input type="radio" name="kolvo" value="3" >3</label>
+            <label><input type="radio" name="kolvo" value="4" >4</label>
           <label>
             <br>Сверхспособности:<br>
-            <select name="sposobn" multiple="multiple">
+            <select name="sposobn[]" multiple="multiple">
             <option value="immortal">бессмертие</option>
             <option value="throughwalls" >прохождение сквозь стены</option>
              <option value="levitation" >левитация</option>
